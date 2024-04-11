09386ccbebc57fa0e3264c4bb3cbcfeff358a165
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-11 23:52:59 +0530

    Implemented React-router-Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import {Outlet} from 'react-router-dom'
 import Header from './components/Header.js'
 import  Footer  from "./components/Footer.js";
 import Homescreen from "./screens/Homescreen.jsx";
@@ -10,7 +11,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1 className="text-center">Welcome to Cartify</h1>
-          <Homescreen/>
+          <Outlet/>
         </Container>
       </main>
       <Footer/>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant="dark" expand="lg" collapseOnSelect justify-content-end>
         <Container>
-          <Navbar.Brand href="/">Cartify</Navbar.Brand>
+          <LinkContainer to="/">
+          <Navbar.Brand >Cartify</Navbar.Brand></LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/login"><FaUser />Sign In</Nav.Link>
-              <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+              <LinkContainer to="/login"><Nav.Link ><FaUser />Sign In</Nav.Link></LinkContainer>
+              <LinkContainer to="/cart"><Nav.Link ><FaShoppingCart />Cart</Nav.Link></LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
