18547e35f2964bf23c375c47a205ffaf6f0354d6
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-14 14:46:41 +0530

    Connected Database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import products from './data/products.js';
 import dotenv from 'dotenv'
+import connectDB from './config/db.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 const app = express()
 
