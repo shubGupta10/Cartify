5ece04aee9388bae2a41bb5fc76586cf373bbd7b
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-17 00:04:35 +0530

    Setup Redux Toolkit

--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -1,33 +1,23 @@
-import { useEffect, useState } from 'react'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product.jsx'
-import axios from 'axios'
+import { useGetProductsQuery } from '../slices/productsApiSlice.js'
 
 const Homescreen = () => {
-
-  const [products, setProducts] = useState([]);
-  
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get("/api/products");
-      setProducts(data);
-    }
-
-    fetchProducts();
-  }, [])
-
-
-
+  const {data: products, isLoading, error} = useGetProductsQuery(); 
   return (
     <>
-    <h1>Latest Product</h1>
-    <Row>
-    {products.map((product) => (
-        <Col key={product._id} sm={12} md={6} lg={6} xl={3} >
-        <Product product={product}/>
-        </Col>
-    ))}
-    </Row>
+    {isLoading ? (
+      <h2>Loading...</h2>
+    ): error ? (<div>{error?.data?.message || error.error}</div>) : (<>
+      <h1>Latest Product</h1>
+      <Row>
+      {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={6} xl={3} >
+          <Product product={product}/>
+          </Col>
+      ))}
+      </Row>
+    </>) }
     </>
   )
 }
