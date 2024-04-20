b6a9f22d972427e0f549f0b8b1f951983e2f5844
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-20 16:09:44 +0530

    Adding Bcryptjs for hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,21 @@ import User from '../model/userModel.js'
 
 
 const authUser = asyncHandler(async (req,res) => {
-    res.send('auth user');
+    const { email, password} = req.body;
+
+    const user = await User.findOne({ email});
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else{
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 const registerUser = asyncHandler(async (req,res) => {
