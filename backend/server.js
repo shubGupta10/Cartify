b9790f3535e68ae26e54be5d650d995702d326f5
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-20 15:53:26 +0530

    Working on userRoutes and userController

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'   
+import userRoutes from './routes/userRoutes.js'
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
+
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +18,7 @@ app.get("/" , (req,res)=> {
 
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
