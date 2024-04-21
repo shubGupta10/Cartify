8c63aab62d3e8fecf3907fdf8463d6cdd66aea84
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-21 15:34:40 +0530

    Auth Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'   
 import userRoutes from './routes/userRoutes.js'
@@ -14,6 +15,9 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+app.use(cookieParser());
 
 app.get("/" , (req,res)=> {
     res.send("API is running")
