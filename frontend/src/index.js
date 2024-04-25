e18df56938920bbb9a6de82111ce1ffdbd43d02a
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-25 19:14:47 +0530

    PrivateRoute for Shipping Address

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PrivateRoute from './components/PrivateRoute.jsx';
 import Homescreen from './screens/Homescreen';
 import ProductPages from './screens/ProductPages';
 import CartScreen from './screens/CartScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import ShippingScreen from './screens/ShippingScreen.jsx';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,7 +24,10 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen/>}/>
       <Route path='/login' element={<LoginScreen/>}/>
       <Route path='/register' element={<RegisterScreen/>}/>
-      <Route path='/shipping' element={<ShippingScreen/>}/>
+      
+      <Route path='' element={<PrivateRoute/>}>
+         <Route path='/shipping' element={<ShippingScreen/>}/>  
+      </Route>
     </Route>
   )
 )
