cc5f8a6eabe5850afb2546523be5232a21e2499d
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-26 18:35:30 +0530

    orderRoutes and orderControllers

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,43 @@
+import asyncHandler from "../middleware/asyncHandler.js"
+import Order from "../model/orderModel.js"
+
+
+const addOrderItems = asyncHandler(async (req, res) => {
+  res.send('add order ietms');
+});
+
+
+
+const getMyOrders = asyncHandler(async (req, res) => {
+    res.send('get my orders');
+});
+
+
+const getOrderById = asyncHandler(async (req, res) => {
+    res.send('get order by id');
+});
+
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    res.send('update order to paid') ;
+});
+
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    res.send('update order to delivered');
+});
+
+
+const getOrders = asyncHandler(async (req, res) => {
+    res.send('get all orders');
+});
+
+export {
+    
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders
+};
