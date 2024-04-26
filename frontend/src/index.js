eb8559aff60cd06af7818f510cda990373264161
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-26 18:14:21 +0530

    PaymentScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import CartScreen from './screens/CartScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import ShippingScreen from './screens/ShippingScreen.jsx';
+import PaymentScreen from './screens/PaymentScreen.jsx';
 
 
 const router = createBrowserRouter(
@@ -27,6 +28,7 @@ const router = createBrowserRouter(
       
       <Route path='' element={<PrivateRoute/>}>
          <Route path='/shipping' element={<ShippingScreen/>}/>  
+         <Route path='/payment' element={<PaymentScreen/>}/>
       </Route>
     </Route>
   )
