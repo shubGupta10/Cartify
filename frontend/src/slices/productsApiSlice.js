560806a61284baa4a32dbb8d3bb5be8c0d52e6f6
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-05-03 18:50:09 +0530

    Product Image Upload

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice.js";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -31,8 +31,15 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 body: data,
             }),
             invalidatesTags: ['Products'],
+        }),
+        uploadProductImage: builder.mutation({
+            query: (data) => ({
+                url: `${UPLOAD_URL}`,
+                method: 'POST',
+                body: data,
+            }),
         })
     }),
 });
 
-export const {useGetProductsQuery, useGetProductDetailsQuery , useCreateProductMutation , useUpdateProductMutation} = productsApiSlice;
+export const {useGetProductsQuery, useGetProductDetailsQuery , useCreateProductMutation , useUpdateProductMutation, useUploadProductImageMutation} = productsApiSlice;
