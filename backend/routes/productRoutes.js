dadf5dfc20aadc48654ddc87d12d0b16b1f8d672
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-05-09 18:45:33 +0530

    Setup for Production

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 const router = express.Router();
 
-import { getProducts , getProductById, createProduct, updatedProduct , deleteProduct, getTopProducts} from '../controllers/productControllers.js';
+import { getProducts , getProductById, createProduct, updateProduct , deleteProduct, getTopProducts} from "../controllers/productControllers.js"
 import {protect, admin} from '../middleware/authMiddleware.js'
 
 router.route("/").get(getProducts).post(protect, admin, createProduct)
 router.get('/top', getTopProducts);
-router.route("/:id").get(getProductById).put(protect, admin, updatedProduct).delete(protect, admin, deleteProduct );
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct );
 
 
 
