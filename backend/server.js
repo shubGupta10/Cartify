ffab8fc495e6921648ef829fd19cff5019c1fbb5
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-05-09 19:23:37 +0530

    Bug fixes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import uploadRoutes from './routes/uploadRoutes.js'
 
 
 dotenv.config();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express()
@@ -22,6 +22,9 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
 
+app.get("/" , (req,res)=> {
+    res.send("API is running")
+});
 
 
 app.use("/api/products", productRoutes);
@@ -32,26 +35,8 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static('/var/data/uploads'));
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-  } else {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-    app.get('/', (req, res) => {
-      res.send('API is running....');
-    });
-  }
-
 app.use(notFound);
 app.use(errorHandler);
 
 
-app.listen(port, () =>
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-  );
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
